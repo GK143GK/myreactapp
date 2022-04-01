091a4252636ab2089ed50d21b6791af6db3b5e36
GK143GK <sgopi.mba@gmail.com>
2022-04-01 09:46:15 +0530

    Completed Frint End App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Recruite } from "./NewResource";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import { Home } from "./Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App=()=>
+{
+    return(
+        <>
+            <Home/>
+            
+        </>
+    )
+   
 }
 
+function Hey()
+{
+    return(<p>Hooks</p>)
+}
+
+export {Hey}
 export default App;
