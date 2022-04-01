729ae20ddb64669e7f782307f88030180a4976cd
GK143GK <sgopi.mba@gmail.com>
2022-04-01 12:55:32 +0530

    Back End Project created

---
+++ crudoperations/src/test/java/myfirst/springboot/backend/crudoperations/CrudoperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package myfirst.springboot.backend.crudoperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudoperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
